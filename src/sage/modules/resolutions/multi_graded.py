61d63f800e1a1949a0a597311b4bde564d95946e
Kwankyu Lee <ekwankyu@gmail.com>
2022-05-13 09:47:36 +0900

    Add resolutions

---
+++ src/sage/modules/resolutions/multi_graded.py
@@ -0,0 +1,15 @@
+"""
+"""
+
+class MultiGradedFreeResolution(FreeResolution):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+
+def multi_graded_free_resolution(S, degrees):
+    pass
+
+
