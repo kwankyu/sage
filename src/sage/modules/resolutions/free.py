458d2b2caf949e0efa2b30f6ad6d56f00b3de610
Kwankyu Lee <ekwankyu@gmail.com>
2022-05-18 00:40:00 +0900

    fix

---
+++ src/sage/modules/resolutions/free.py
@@ -0,0 +1,16 @@
+"""
+Free resolutions
+
+AUTHORS:
+
+- Kwankyu Lee (2022-05-13): initial implementation
+"""
+
+from sage.structure.sage_object import SageObject
+
+
+class FreeResolution(SageObject):
+    """
+    Base class of free resolutions.
+    """
+    pass
